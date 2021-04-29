b98ed36a92c4298b46b1398c1c8a266062f92227
Jadivam <Jmont261@fiu.edu>
2021-04-29 18:08:34 -0400

    add navbar and navbar links

---
+++ client/src/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar-container">
+            <ul className="navbar-list-links">
+                <li className="navbar-link">
+                    Write a Review
+                </li>
+                <li className="navbar-link">
+                    Contact
+                </li>
+                <li className="navbar-link">
+                    About
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
