98740432912ddf21334c3b1d18806432654e4457
Jadivam <Jmont261@fiu.edu>
2021-05-05 01:27:29 -0400

    change syntax error from class to className

--- client/src/Searchbar/Searchbar.jsx
+++ client/src/Searchbar/Searchbar.jsx
@@ -7,7 +7,7 @@ const Searchbar = () => {
                 <div className="dropdown-first-container">
                 <span className="dropdown-first-container-text">Find</span>
                 <span className="dropdown searchbar-placeholder">resume, projects, about...</span>
-                <div class="dropdown-content">
+                <div className="dropdown-content">
                 <p>Hello World!</p>
                 </div>
                 </div>
@@ -15,7 +15,7 @@ const Searchbar = () => {
                 <span className="dropdown-second-container-text">Near</span>
                 <span className="dropdown">Miami, FL</span>
                 <div class="dropdown-search-icon-container">
-                    <button class="dropdown-search-icon-button">
+                    <button className="dropdown-search-icon-button">
                     <SearchIcon className="dropdown-search-icon" />
                     </button>
                 </div>
