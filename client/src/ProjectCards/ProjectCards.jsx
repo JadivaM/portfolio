6b22a144c7869879dc8a7e54ebc5f121787421b0
Jadivam <Jmont261@fiu.edu>
2021-05-05 23:45:12 -0400

    add external link to projects

--- client/src/ProjectCards/ProjectCards.jsx
+++ client/src/ProjectCards/ProjectCards.jsx
@@ -4,17 +4,23 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import yelpstars from '../YelpImages/yelp-stars.png';
 import Projects from '../data/data.json';
+import {Link} from 'react-router-dom';
 
 const ProjectCards = () => {
+
+
     return (
         <div className="home-project-cards-container">
   {Projects.map((project) =>  {
     return (
       <Card variant="outlined" className="project-cards">
       <CardContent>
+      <Link
+      to={{pathname: `${project.link}`}} target="_blank" style={{textDecoration: 'none'}}>
         <p className="project-card-title">
           {project.name}
         </p>
+      </Link>
         <img className="project-card-stars" src={yelpstars} alt="red stars rating the project"/>
          <p className="project-card-category">{project.desc}</p>
         <p className="project-card-location">
